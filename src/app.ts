47916aa38dc2d81324a762ba7a6357ec6c84fe29
matheus.marques <matheus.marques@limberchef.com.br>
2024-11-11 18:03:03 -0300

    configurations of initialize

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,23 @@
 import express from 'express';
+import {Pool} from "pg";
+import usersRoute from './routes/user.route';
+import {environment} from "./config/environment";
+
+const app = express();
+
+
+const pool = new Pool({
+    connectionString: environment.dataBaseUrl
+});
+
+pool.connect()
+    .then(() => console.log('Conexão com o banco de dados efetuada com sucesso!'))
+    .catch(err => console.error(`Erro ao efetuar conexão com o banco de dados`, err));
+
+
+app.use(express.json());
+
+app.use('/users', usersRoute)
+
+export default app;
 
---
+++ src/config/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    dataBaseUrl: process.env.DATABASE_URL,
+}
